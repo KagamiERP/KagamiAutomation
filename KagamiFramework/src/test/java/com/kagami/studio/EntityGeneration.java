3ae55d65d1acd11b1dfa1c688322eddaf9f0fe42
Manish093 <manish.anand@kagamierp.com>
2016-09-21 12:50:26 +0530

    Studio Login

--- KagamiFramework/src/test/java/com/kagami/studio/EntityGeneration.java
+++ KagamiFramework/src/test/java/com/kagami/studio/EntityGeneration.java
@@ -1,10 +1,43 @@
 package com.kagami.studio;
 
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.kagami.library.Global;
+import com.kagami.testconfig.BrowserSelection;
 
 public class EntityGeneration {
-	
+	public WebDriver driver;
+	public EntityGenerationBase entityGenerationBase;
+
+	@BeforeClass
+	public void browserSelection() throws EncryptedDocumentException, AddressException, InvalidFormatException, IOException, InterruptedException, MessagingException
+	{
+		BrowserSelection browserSelection = new BrowserSelection();
+		driver = browserSelection.browserType(driver, Global.sBrowserType);
+	}
+
+    @Test(priority = 0)
+	public void verifyStudioSignIn()
+	{
+		entityGenerationBase = new EntityGenerationBase(driver);
+		entityGenerationBase.studioLogin();
+	}
 	
 
+
+	@AfterClass
+	public void browserShutDown()
+	{
+		driver.close();
+	}
 }
--- KagamiFramework/src/test/java/com/kagami/studio/EntityGenerationBase.java
+++ KagamiFramework/src/test/java/com/kagami/studio/EntityGenerationBase.java
@@ -1,13 +1,58 @@
 package com.kagami.studio;
+import java.io.File;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import com.kagami.library.ExtentManager;
+import com.kagami.library.GenericMethods;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class EntityGenerationBase {
+
 	ExtentReports extent;
 	ExtentTest test;
 	public  WebDriver driver;
+	static Logger log = Logger.getLogger("EntityGenerationBase.class");
+	GenericMethods genericMethods = new GenericMethods();
+
+	By uname = By.id("inputUsernme");
+	By password = By.id("inputPassword");
+
+	String userName = "admin";
+	String passWord = "admin";
+
+	public EntityGenerationBase(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+
+	public void studioLogin()
+	{
+		try{
+			extent = ExtentManager.Instance();
+			extent.loadConfig(new File("C:\\extent\\config.xml"));
+			test = extent.startTest("Login", "Login to Kagami Studio");
+			driver.findElement(uname).sendKeys(userName);
+			driver.findElement(password).sendKeys(passWord);
+			test.log(LogStatus.PASS, "Login Successful");
+		}
+
+		catch(Exception e)
+		{
+			test.log(LogStatus.INFO, test.addScreenCapture(ExtentManager.CaptureScreen(driver)));
+			test.log(LogStatus.FAIL, ExceptionUtils.getStackTrace(e));
+		}
+
+		finally {
+			extent.endTest(test);
+			extent.flush();
+		}
+
+	}
 
 }
